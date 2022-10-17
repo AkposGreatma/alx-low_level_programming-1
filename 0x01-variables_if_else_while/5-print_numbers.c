738710bb7b0a575111e67d20f7e80a900521cbf8
Greatma <oghenemarogreat77@gmail.com>
2022-10-16 19:58:08 -0700

    For..numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 10; b++)
+	{
+	printf("%d", b);
+	}
+	printf("\n");
+	return (0);
+}
