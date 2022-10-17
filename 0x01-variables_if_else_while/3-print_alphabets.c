e4a49ca5ade7e836f93f5796266e6cdb630e2a57
Greatma <oghenemarogreat77@gmail.com>
2022-10-16 19:15:25 -0700

    Uppercase and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0
+*/
+int main(void)
+{
+	char g;
+	char o;
+
+	for (g = 'a'; g <= 'z'; g++)
+	{
+	putchar(g);
+	}
+	for (o = 'A'; o <= 'Z'; o++)
+	{
+	putchar(o);
+	}
+
+	return (0);
+}
