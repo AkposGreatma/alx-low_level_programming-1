527649a67d8d7d24e6e42a7779fd6deb420602c9
Greatma <oghenemarogreat77@gmail.com>
2022-10-19 06:42:35 -0700

    This is easy

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @i: first integer to add
+ * @j: second integer to add
+ * Return: the value of addition
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
