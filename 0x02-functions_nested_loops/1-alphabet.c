72e23488da9bc54952968aac3455e0880d26289d
Greatma <oghenemarogreat77@gmail.com>
2022-10-18 21:02:33 -0700

    Lowcase alphabetic order

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Print alphabets
- * Return: Always 0
+ * print_alphabet - Print alphabets
  */
 void print_alphabet(void)
 {
@@ -13,5 +12,4 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
