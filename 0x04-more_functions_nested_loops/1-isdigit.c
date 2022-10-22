0cedcf16f9e52161f900fc98339556485134408b
Greatma <oghenemarogreat77@gmail.com>
2022-10-21 17:16:38 -0700

    Know your digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _isdigit - checks for digits
+  * @c: the variable
+  *
+  * Return: 0 or 1
+  */
+int _isdigit(int c)
+{
+	if (c < 10)
+		return (1);
+	else
+		return (0);
+}
